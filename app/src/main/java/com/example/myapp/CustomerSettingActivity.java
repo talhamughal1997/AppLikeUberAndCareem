c9a8b3b84409b51eb6125530c1cec6c4a3fb5f0f
talha mughal <talhamughal1997@gmail.com>
2019-03-27 15:35:50 +0500

    Upload data Firebase Storage include images and Strings

--- app/src/main/java/com/example/myapp/CustomerSettingActivity.java
+++ app/src/main/java/com/example/myapp/CustomerSettingActivity.java
@@ -1,19 +1,36 @@
 package com.example.myapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,11 +38,13 @@ public class CustomerSettingActivity extends AppCompatActivity implements View.O
 
     EditText txtName, txtPhone;
     Button btnConfirm, btnBack;
+    ImageView mProfileImage;
 
     FirebaseAuth mFirebaseAuth;
     DatabaseReference mCustomerDatabase;
 
-    String userId, mName, mPhone;
+    String userId, mName, mPhone, mProfileImageUrl;
+    private Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +62,7 @@ public class CustomerSettingActivity extends AppCompatActivity implements View.O
     private void ViewsListeners() {
         btnConfirm.setOnClickListener(this);
         btnBack.setOnClickListener(this);
+        mProfileImage.setOnClickListener(this);
     }
 
     private void ViewsInit() {
@@ -50,6 +70,7 @@ public class CustomerSettingActivity extends AppCompatActivity implements View.O
         txtPhone = findViewById(R.id.edttxt_Phone);
         btnConfirm = findViewById(R.id.btnConfirm);
         btnBack = findViewById(R.id.btnBack);
+        mProfileImage = findViewById(R.id.profileImage);
     }
 
     @Override
@@ -61,6 +82,12 @@ public class CustomerSettingActivity extends AppCompatActivity implements View.O
             }
             case R.id.btnBack: {
                 finish();
+                break;
+            }
+            case R.id.profileImage: {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
                 break;
             }
         }
@@ -81,6 +108,10 @@ public class CustomerSettingActivity extends AppCompatActivity implements View.O
                         mPhone = map.get("phone").toString();
                         txtPhone.setText(mPhone);
                     }
+                    if (map.get("ProfileImageUrl") != null) {
+                        mProfileImageUrl = map.get("ProfileImageUrl").toString();
+                        Glide.with(getApplicationContext()).load(mProfileImageUrl).into(mProfileImage);
+                    }
                 }
 
             }
@@ -97,10 +128,69 @@ public class CustomerSettingActivity extends AppCompatActivity implements View.O
         mPhone = txtPhone.getText().toString();
 
         Map userInfo = new HashMap();
-        userInfo.put("name",mName);
-        userInfo.put("phone",mPhone);
+        userInfo.put("name", mName);
+        userInfo.put("phone", mPhone);
         mCustomerDatabase.updateChildren(userInfo);
 
-        finish();
+        if (resultUri != null) {
+            saveImage();
+
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            final Uri imageUri = data.getData();
+            resultUri = imageUri;
+            mProfileImage.setImageURI(resultUri);
+        }
+    }
+
+    private void saveImage() {
+        StorageReference filePath = FirebaseStorage.getInstance().getReference().child("ProfileImageUrl").child(userId);
+        Bitmap bitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), resultUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+        byte[] data = baos.toByteArray();
+        final UploadTask uploadTask = filePath.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                finish();
+                return;
+
+            }
+        });
+
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                final Task<Uri> downloadUrl = uploadTask.getResult().getMetadata().getReference().getDownloadUrl();
+                downloadUrl.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Log.d("onupload", "onSuccess: ");
+                        //Task<Uri> downloadUrl = taskSnapshot.getStorage().getDownloadUrl();
+                        Map newImage = new HashMap();
+                        newImage.put("ProfileImageUrl", uri.toString());
+                        mCustomerDatabase.updateChildren(newImage);
+                        finish();
+                        return;
+                    }
+                });
+
+            }
+        });
     }
 }
